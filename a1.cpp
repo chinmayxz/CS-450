5403e15b6f0b2b66b1c2aacac2c67c8d47802d55
Chinmay <chinmayxz@gmail.com>
2025-06-11 15:19:58 -0600

    Initial commit: Added C++ console

---
+++ a1.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+#include <string>
+using namespace std; 
+
+int main (){
+    string first_name = "Apple"; 
+    string last_name = "Tree"; 
+    string address = "123 Pine St"; 
+    string city = "Boulder"; 
+    string zipcode = "80301"; 
+     cout << "Name: " << first_name << " " << last_name << endl;
+     cout << "Address: " << address << " " << city << " " << zipcode << endl;
+     return 0;
+}
