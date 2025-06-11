9adec50e264d1e182b9cb1fceb52d3e0796e47c0
Chinmay <chinmayxz@gmail.com>
2025-06-11 16:26:23 -0600

    updated

---
+++ a2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Welcome to this C++ Program" << endl;
+    cout << "I have corrected all errors for this program." << endl;
+    // Wait For Output Screen (if needed, uncomment next line)
+    // cin.get(); // Wait for user input before closing
+    return 0;
+}
